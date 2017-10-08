f452d8c3af7e95cf7f29ff4a4b2858fab6afcc85
cfregly <chris@fregly.com>
2017-10-07 22:19:01 -0400

    renamed to _handler to differentiate between similarly-named packages (ie. kafka)

--- pipeline_logger/test.py
+++ pipeline_logger/test.py
@@ -1,7 +1,8 @@
 from pipeline_logger import log
 import logging
 import json
-from pipeline_logger.kafka import KafkaHandler
+from pipeline_logger.kafka_handler import KafkaHandler
+from pipeline_logger.tensorboard_handler import TensorboardHandler
 
 _logger = logging.getLogger('test_logger')
 _logger.setLevel(logging.INFO)
@@ -11,6 +12,9 @@ _logger.addHandler(_logger_stream_handler)
 
 _logger_kafka_handler = KafkaHandler(hosts_list='localhost:9092', topic='prediction-inputs')
 _logger.addHandler(_logger_kafka_handler)
+
+_logger_tensorboard_handler = TensorboardHandler(logdir_path='/tmp/pipeline_logger.tensorboard_handler')
+_logger.addHandler(_logger_tensorboard_handler)
 
 @log(logger=_logger, 
      labels={'a_label_key': 'a_label_value'})

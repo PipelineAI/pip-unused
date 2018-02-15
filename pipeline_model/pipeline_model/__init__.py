6f5ea44caf3e75a622192f4dba1aaa381658f3ea
cfregly <chris@fregly.com>
2018-02-14 23:59:04 -0800

    fixed minor bug

--- pipeline_model/pipeline_model/__init__.py
+++ pipeline_model/pipeline_model/__init__.py
@@ -5,7 +5,7 @@ import tensorflow as tf
 # These are generated from the TF serving source.
 from . import model_pb2, predict_pb2, prediction_service_pb2
 
-__version__ = "1.0.3"
+__version__ = "1.0.4"
 
 class TensorFlowServingModel():
     
@@ -30,15 +30,15 @@ class TensorFlowServingModel():
         stub = prediction_service_pb2.beta_create_PredictionService_stub(channel)
        
         # Transform input str::nparray dict into TensorFlow PredictRequest/tensors
-        for input_str_key, _ in input_str_nparray_dict.items():
-            input_tensor = tf.make_tensor_proto(input_str_nparray_dict[input_str_key],
-                                                dtype=tf.float32)
-            tf_request.inputs[input_str_key].CopyFrom(input_tensor)
-
         tf_request = predict_pb2.PredictRequest()
         tf_request.model_spec.name = self._model_name
         # We assume only a single version per model is running in this model server.
         # tf_request.model_spec.version.value = ...
+
+        for input_str_key, _ in input_str_nparray_dict.items():
+            input_tensor = tf.make_tensor_proto(input_str_nparray_dict[input_str_key],
+                                                dtype=tf.float32)
+            tf_request.inputs[input_str_key].CopyFrom(input_tensor)
 
         # Call TensorFlow Serving Predict
         response = stub.Predict(tf_request, self._timeout)
